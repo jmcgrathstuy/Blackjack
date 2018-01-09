0737b9b658080d27e9ea33d5da2ab4517f3a4035
jmcgrathstuy <jmcgrath@stuy.edu>
2018-01-09 15:18:04 -0500

    Added isAce method to Card.java, created Player.java

---
+++ BlackJack/Card.java
@@ -0,0 +1,34 @@
+public class Card {
+    private int Value;
+    private String Name;
+
+    public Card (String name , int worth) {
+
+
+	if(worth > 11) {
+	    throw new IllegalArgumentException() ;
+    }
+	setName(name);
+	setValue(worth);
+
+
+    }
+
+    public String getName(){
+	return Name;
+    }
+
+    public void setName(String  temp) {
+	Name = temp;
+    }
+
+    public int  getValue() {
+	return Value; }
+
+    public void setValue(int  temp) {
+	Value = temp;}
+
+    public boolean isAce(){
+        return Name = "Ace";
+    }
+}
