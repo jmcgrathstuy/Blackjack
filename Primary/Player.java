700b5a7f945ec553bd3346e664e3457d62b606d5
Chicken-Ninja <eko1@stuy.edu>
2018-01-15 21:44:24 -0500

    Added comments to the Player class

--- Primary/Player.java
+++ Primary/Player.java
@@ -1,22 +1,19 @@
 
 public class Player extends Person{
 
-    //private int CardCount = 0;
-    //ArrayList<Card> Hand = new ArrayList<Card>(0);
-    //private int Total = 0;
-    private String Name;
-    private int Bet;
-    private int Money;
-	private int HandValue;
-    private Boolean stand = false; 
+    private String Name; //Name of the player
+    private int Bet; //The amount of betting money that the person is going to spend in a round 
+    private int Money;//The amount of total money that the person has.
+    private int HandValue; //The added value of all the cards in that person's hand.
+    private Boolean stand = false; //Helps Casino.Gui determine turns. 
     
 
-
+    //Constructor asks for the Name and starting money
     public Player(String inName, int StartingMoney){
 	setName(inName);
 	setMoney(StartingMoney);
     }
-
+    //Accessor methods /Mutator Methods
     public int getBet() {
 	return Bet;}
     
@@ -29,7 +26,7 @@ public class Player extends Person{
 
     public void setBet(int temp) {
 	Bet = temp;}
-
+    //This double down is meant to draw from the deck, check if the player busts, then return true if they didn't and false if they did. 
     public Boolean doubleDown(Deck thisDeck) {
 	Draw(thisDeck);
 	if(checkBust() == false)
@@ -43,7 +40,7 @@ public class Player extends Person{
     
     
     
-
+    //More accessors /Mutators
 
     public String getName(){
 	return Name;
@@ -53,12 +50,12 @@ public class Player extends Person{
 	Name = newName;
     }
     
-
+    //Hit simply draws from the deck. Done for simplicity sake/Code structure was later revised to not really include it. 
     public void hit( Deck drawDeck){
 	Draw(drawDeck);
 	
     }
-
+    //This sets a boolean value in the player to true, which helps CasinoGUi.java tell when a player is finished with their turn, and their cards ready to be evaluated. 
     public void stand(){
 	stand  = true;}
     
