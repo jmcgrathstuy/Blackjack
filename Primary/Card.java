4506b641f60300ceea0184d49f40fd682120d9e3
Chicken-Ninja <eko1@stuy.edu>
2018-01-15 22:00:47 -0500

    Added comments and fixed an argument exception

--- Primary/Card.java
+++ Primary/Card.java
@@ -1,11 +1,11 @@
 public class Card {
-    private int Value;
-    private String Name;
+    private int Value; //The amount that the card is worth 
+    private String Name;//The name of the card 
 
     public Card (String name , int worth) {
 
-
-	if(worth > 11) {
+	//If the worth is inputted as being above 11 (impossible, because 11 is the highest number, and less than 0,  because it is impossible, return an error. 
+	if(worth > 11|| worth < 0) {
 	    throw new IllegalArgumentException() ;
     }
 	setName(name);
@@ -13,7 +13,7 @@ public class Card {
 
 
     }
-
+    //mutators and accessors
     public String getName(){
 	return Name;
     }
@@ -23,11 +23,11 @@ public class Card {
     }
 
     public int  getValue() {
-	return Value; }
-
+s	return Value; }
+    
     public void setValue(int  temp) {
 	Value = temp;}
-
+    //Returns true if the card is an ace
     public boolean isAce(){
         return Name == "Ace";
     }
