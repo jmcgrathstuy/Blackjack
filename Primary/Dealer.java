eb5c236168bf454ca33c0344e717ca6fcfa2f1dd
Chicken-Ninja <eko1@stuy.edu>
2018-01-15 21:59:00 -0500

    Added comments to the dealer class

--- Primary/Dealer.java
+++ Primary/Dealer.java
@@ -3,14 +3,14 @@ public class Dealer extends Person {
 
     
 	
-
+    
     public void hit(Deck thisDeck) {
 	while(getTotal() < 17) 
 	    {Draw(thisDeck);}
     
     }
 	
-	
+    //checkCool basically checks if the dealer should draw or not, because the dealer has a set of rules that they must abide by. 16 and lower means they hit, while 17 and above means they stand. 
 	public Boolean checkCool() {
 		int temp = getTotal();
 		if (temp > 16){
@@ -28,7 +28,7 @@ public class Dealer extends Person {
 	    return true;}
 		return false;
 	}
-	
+    //fullTurn has the dealer draw until his rules no longer permit him to do so. 
 	public void fullTurn(Deck thisDeck){
 		while(!checkCool()){
 			Draw(thisDeck);
