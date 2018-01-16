7574b56ff4f76542a472038140f72e3e320598cb
Chicken-Ninja <eko1@stuy.edu>
2018-01-15 22:02:23 -0500

    removed a typo

--- Primary/Card.java
+++ Primary/Card.java
@@ -22,8 +22,8 @@ public class Card {
 	Name = temp;
     }
 
-    public int  getValue() {
-s	return Value; }
+    public int getValue() {
+	return Value ;}
     
     public void setValue(int  temp) {
 	Value = temp;}
