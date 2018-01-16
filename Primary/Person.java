4790d4fc2e7e5b165dc79cae24a53fa62d6e39e4
Chicken-Ninja <eko1@stuy.edu>
2018-01-15 21:51:01 -0500

    Finished adding comments to the person class

--- Primary/Person.java
+++ Primary/Person.java
@@ -2,17 +2,17 @@ import java.util.*;
 
 public abstract class Person{
     
-    private int CardCount = 0; 
+    private int CardCount = 0; //This shows the number of cards in the person's hand
     
-    ArrayList <Card> Hand = new ArrayList<Card>(0);
+    ArrayList <Card> Hand = new ArrayList<Card>(0); //This represents the person's hand, with an array list that is filled with Cards (originally, size is set to 0)
 
-    private int Total = 0;
+    private int Total = 0; //Total value of all the combined cards in the Person's hand. 
 
-    private String name; 
+    private String name; //The Persons's name. 
 
 
 
-    public Boolean checkWin(Person dude) {
+    public Boolean checkWin(Person dude) { //This method essentially checks to see if the person has either : Gotten blackjack, hit a five card Charlie, or has beaten the Dealer/ other player. 
 		if (getTotal() == 21){
 			return true;
 		}
@@ -24,7 +24,11 @@ public abstract class Person{
 		}
 		return false;
 	} 
-
+     //Checkbust checks whether the player's hand has exceeded 21, and if so, tries to reduce Aces from 11 to 1
+	//If reducing Aces can reduce the hand's value below 21 or is not needed, checkbust returns false.
+	//If the hand's value exceeds 21 no matter what, checkBust returns true.
+	//
+	
     public Boolean checkBust() {
 		int temp = getTotal();
 		if (temp > 21){
@@ -43,12 +47,8 @@ public abstract class Person{
 	    return true;}
 		return false;
 	}
-	//Checkbust checks whether the player's hand has exceeded 21, and if so, tries to reduce Aces from 11 to 1
-	//If reducing Aces can reduce the hand's value below 21 or is not needed, checkbust returns false.
-	//If the hand's value exceeds 21 no matter what, checkBust returns true.
-	//
 	
-
+    //Intentional Draw selects a card from the deck with the given name, and draws that. This is meant to be here for testing /demoing purposes. 
     public void intentionalDraw(Deck thisDeck,String name){
 		Card copyCard = thisDeck.NameDraw(name); 
 		Hand.add(copyCard); 
@@ -56,14 +56,14 @@ public abstract class Person{
 		setCardCount(getCardCount() + 1);
     }
     
-    
+    //This is the Draw method, where a person draws from the deck, has his hand , his total, and his card count updated, while having the Deck subtract that card from it's supply of cards. 
     public void Draw(Deck drawDeck) {
 		Card copyCard = drawDeck.draw();
 		Hand.add(copyCard);
 		setTotal(getTotal() + copyCard.getValue());
         setCardCount( getCardCount() + 1);
     }
-
+    //HasAce is to determine whether or not the person has an ace in their hand. This is important because aces can be treated as either 11 or 1 in Blackjack, making it hard to check for busts/wins.Returns True if there is an ace, and false if there is none. 
     public Boolean hasAce() {
         for(int counter = 0; counter < Hand.size(); counter++ ) {
 	    if(Hand.get(counter).getName() == "Ace")
@@ -71,16 +71,16 @@ public abstract class Person{
 	}
 	return false;
     }
-
+    
     abstract void hit( Deck drawDeck);
-
+    //This resets the player's hand, as well as his total value, as well as the number of cards in his hand. 
     public void reset() {
 		Hand = new ArrayList<Card> (0);
 		Total = 0;
 		CardCount = 0;
 	}
 	
-
+    //Accessors/Mutators
     public int getCardCount () { 
 	return CardCount;
 	}
@@ -105,7 +105,7 @@ public abstract class Person{
 		Hand = new ArrayList<Card> (0);
 		setTotal(0);
 	}
-
+    //This method return true, if the person has managed to draw a fiveCard Charlie, meaning that they have successfully drawn 5 cards without Busting. Returns true if they have, and false if not. 
     public Boolean fiveCC () {
 		if (getCardCount() >= 5 && checkBust() == false){
 			return true;
