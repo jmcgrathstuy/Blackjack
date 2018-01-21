b8995eb49787d12da628e6d11b0a8ca93ce3425c
jmcgrathstuy <jmcgrath@stuy.edu>
2018-01-21 14:35:10 -0500

    Fixed some card names and update the Card class's constructor, variables, and methods to support imageIcons. also Updated Deck to reflect these changes.

--- Primary/Card.java
+++ Primary/Card.java
@@ -1,8 +1,14 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
 public class Card {
     private int Value; //The amount that the card is worth 
     private String Name;//The name of the card 
+	private ImageIcon face;//The image used for displaying the card
+	private String facePath;//String containing the path used to set the face image
 
-    public Card (String name , int worth) {
+    public Card (String name , int worth, String imagePath) {
 
 	//If the worth is inputted as being above 11 (impossible, because 11 is the highest number, and less than 0,  because it is impossible, return an error. 
 	if(worth > 11|| worth < 0) {
@@ -10,6 +16,8 @@ public class Card {
     }
 	setName(name);
 	setValue(worth);
+	face = new ImageIcon( imagePath);
+	facePath = imagePath;
 
 
     }
@@ -31,4 +39,24 @@ public class Card {
     public boolean isAce(){
         return Name == "Ace";
     }
+	
+	public void setFace(ImageIcon newFace){
+		face = newFace;
+	}
+	
+	public void setFace(String imagePath){
+		face = new ImageIcon( imagePath);
+	}
+	
+	public ImageIcon getFace(){
+		return face;
+	}
+	
+	public void setFacePath(String newFacePath){
+		facePath = newFacePath;
+	}
+	
+	public String getFacePath(){
+		return facePath;
+	}
 }
--- Primary/Deck.java
+++ Primary/Deck.java
@@ -6,58 +6,58 @@ public class Deck{
     ArrayList <Card> data = new ArrayList<Card>(52);
 	Random rand = new Random();
     //This is essentially here to define every single card in a deck(not including suits)
-	Card two1 = new Card("Two" , 2);
-	Card two2 = new Card("Two" , 2);
-	Card two3 = new Card("Two" , 2);
-	Card two4 = new Card("Two" , 2);
-	Card three1 = new Card("Three" , 3);
-	Card three2 = new Card("Three" , 3);
-	Card three3 = new Card("Three" , 3);
-	Card three4 = new Card("Three" , 3);
-	Card four1 = new Card("Four" , 4);
-	Card four2 = new Card("Four" , 4);
-	Card four3 = new Card("Four" , 4);
-	Card four4 = new Card("Four" , 4);
-	Card five1 = new Card("Five" , 5);
-	Card five2 = new Card("Five" , 5);
-	Card five3 = new Card("Five" , 5);
-	Card five4 = new Card("Five" , 5);
-	Card six1 = new Card("Six" , 6);
-	Card six2 = new Card("Six" , 6);
-	Card six3 = new Card("Six" , 6);
-	Card six4 = new Card("Six" , 6);
-	Card seven1 = new Card("Seven" , 7);
-	Card seven2 = new Card("Seven" , 7);
-	Card seven3 = new Card("Seven" , 7);
-	Card seven4 = new Card("Seven" , 7);
-	Card eight1 = new Card("Eight" , 8);
-	Card eight2 = new Card("Eight" , 8);
-	Card eight3 = new Card("Eight" , 8);
-	Card eight4 = new Card("Eight" , 8);
-	Card nine1 = new Card("Nine" , 9);
-	Card nine2 = new Card("Nine" , 9);
-	Card nine3 = new Card("Nine" , 9);
-	Card nine4 = new Card("Nine" , 9);
-	Card ten1 = new Card("Ten" , 10);
-	Card ten2 = new Card("Ten" , 10);
-	Card ten3 = new Card("Ten" , 10);
-	Card ten4 = new Card("Ten" , 10);
-	Card Jack1 = new Card("Jack" , 10);
-	Card Jack2 = new Card("Jack" , 10);
-	Card Jack3 = new Card("Jack" , 10);
-	Card Jack4 = new Card("Jack" , 10);
-	Card Queen1 = new Card("Queen" , 10);
-	Card Queen2 = new Card("Queen" , 10);
-	Card Queen3 = new Card("Queen" , 10);
-	Card Queen4 = new Card("Queen" , 10);
-	Card King1 = new Card("King" , 10);
-	Card King2 = new Card("King" , 10);
-	Card King3 = new Card("King" , 10);
-	Card King4 = new Card("King" , 10);
-	Card Ace1 = new Card("Ace" , 11);
-	Card Ace2 = new Card("Ace" , 11);
-	Card Ace3 = new Card("Ace" , 11);
-	Card Ace4 = new Card("Ace" , 11);
+	Card two1 = new Card("Two" , 2, "cardImages/twoClub");
+	Card two2 = new Card("Two" , 2, "cardImages/twoSpade");
+	Card two3 = new Card("Two" , 2, "cardImages/twoHeart");
+	Card two4 = new Card("Two" , 2, "cardImages/twoDiamond");
+	Card three1 = new Card("Three" , 3, "cardImages/threeClub");
+	Card three2 = new Card("Three" , 3, "cardImages/threeSpade");
+	Card three3 = new Card("Three" , 3, "cardImages/threeHeart");
+	Card three4 = new Card("Three" , 3, "cardImages/threeDiamond");
+	Card four1 = new Card("Four" , 4, "cardImages/fourClub");
+	Card four2 = new Card("Four" , 4, "cardImages/fourSpade");
+	Card four3 = new Card("Four" , 4, "cardImages/fourHeart");
+	Card four4 = new Card("Four" , 4, "cardImages/fiveDiamond");
+	Card five1 = new Card("Five" , 5, "cardImages/fiveClub");
+	Card five2 = new Card("Five" , 5, "cardImages/fiveSpade");
+	Card five3 = new Card("Five" , 5, "cardImages/fiveHeart");
+	Card five4 = new Card("Five" , 5, "cardImages/sixDiamond");
+	Card six1 = new Card("Six" , 6, "cardImages/sixClub");
+	Card six2 = new Card("Six" , 6, "cardImages/sixSpade");
+	Card six3 = new Card("Six" , 6, "cardImages/sixHeart");
+	Card six4 = new Card("Six" , 6, "cardImages/sixDiamond");
+	Card seven1 = new Card("Seven" , 7, "cardImages/sevenClub");
+	Card seven2 = new Card("Seven" , 7, "cardImages/sevenSpade");
+	Card seven3 = new Card("Seven" , 7, "cardImages/sevenHeart");
+	Card seven4 = new Card("Seven" , 7, "cardImages/sevenDiamond");
+	Card eight1 = new Card("Eight" , 8, "cardImages/eightClub");
+	Card eight2 = new Card("Eight" , 8, "cardImages/eightSpade");
+	Card eight3 = new Card("Eight" , 8, "cardImages/eightHeart");
+	Card eight4 = new Card("Eight" , 8, "cardImages/eightDiamond");
+	Card nine1 = new Card("Nine" , 9, "cardImages/nineClub");
+	Card nine2 = new Card("Nine" , 9, "cardImages/nineSpade");
+	Card nine3 = new Card("Nine" , 9, "cardImages/nineHeart");
+	Card nine4 = new Card("Nine" , 9, "cardImages/nineDiamond");
+	Card ten1 = new Card("Ten" , 10, "cardImages/tenClub");
+	Card ten2 = new Card("Ten" , 10, "cardImages/tenSpade");
+	Card ten3 = new Card("Ten" , 10, "cardImages/tenHeart");
+	Card ten4 = new Card("Ten" , 10, "cardImages/tenDiamond");
+	Card Jack1 = new Card("Jack" , 10, "cardImages/jackClub");
+	Card Jack2 = new Card("Jack" , 10, "cardImages/jackSpade");
+	Card Jack3 = new Card("Jack" , 10, "cardImages/jackHeart");
+	Card Jack4 = new Card("Jack" , 10, "cardImages/jackDiamond");
+	Card Queen1 = new Card("Queen" , 10, "cardImages/queenClub");
+	Card Queen2 = new Card("Queen" , 10, "cardImages/queenSpade");
+	Card Queen3 = new Card("Queen" , 10, "cardImages/queenHeart");
+	Card Queen4 = new Card("Queen" , 10, "cardImages/queenDiamond");
+	Card King1 = new Card("King" , 10, "cardImages/kingClub");
+	Card King2 = new Card("King" , 10, "cardImages/kingSpade");
+	Card King3 = new Card("King" , 10, "cardImages/kingHeart");
+	Card King4 = new Card("King" , 10, "cardImages/kingDiamond");
+	Card Ace1 = new Card("Ace" , 11, "cardImages/aceClub");
+	Card Ace2 = new Card("Ace" , 11, "cardImages/aceSpade");
+	Card Ace3 = new Card("Ace" , 11, "cardImages/aceHeart");
+	Card Ace4 = new Card("Ace" , 11, "cardImages/aceDiamond");
 
 
     
@@ -123,7 +123,7 @@ public class Deck{
     public Card draw () {
 		int whichSpot = rand.nextInt(data.size());
 		Card pickedCard = data.get(whichSpot);
-		Card copyCard = new Card( pickedCard.getName(), pickedCard.getValue());
+		Card copyCard = new Card( pickedCard.getName(), pickedCard.getValue(), pickedCard.getFacePath());
 		data.remove(whichSpot);
 		return copyCard;
 	}
@@ -132,7 +132,7 @@ public class Deck{
     public Card NameDraw(String taco) {
 	for(int counter = 0; counter < cardsLeft(); counter++ ){
 	    if(data.get(counter).getName() == taco){
-		Card copyCard = new Card(data.get(counter).getName(),data.get(counter).getValue());
+		Card copyCard = new Card(data.get(counter).getName(),data.get(counter).getValue(), data.get(counter).getFacePath());
 		data.remove(counter);
 		return copyCard;
 	    }
